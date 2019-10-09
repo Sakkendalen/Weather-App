37e7a09c0cb804405c5af60aca8843b90f91daea
Saku Tynjälä <c7stynja@kc408mac03.tamk.ad>
2019-10-09 13:22:05 +0300

    init failure. remove project inside project

---
+++ WeatherApp/DataController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Saku Tynjälä on 08/10/2019.
+//  Copyright © 2019 Saku Tynjälä. All rights reserved.
+//
+
+import UIKit
+
+class DataController {
+    
+    
+}
